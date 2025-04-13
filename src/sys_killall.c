5eabeda8887884f53f22d5778a18df9d30ac551f
ndkhoi13505 <ndkhoi13505@gmail.com>
2025-04-13 10:27:06 +0700

    Update sys_killall

--- src/sys_killall.c
+++ src/sys_killall.c
@@ -8,73 +8,109 @@
  * for the sole purpose of studying while attending the course CO2018.
  */
 
-#include "common.h"
-#include "syscall.h"
-#include "stdio.h"
-#include "libmem.h"
-
-#include "queue.h" //! include more
-#include <string.h> 
-#include <stdlib.h>  
-int __sys_killall(struct pcb_t *caller, struct sc_regs *regs)
-{
-    char proc_name[100];
-    uint32_t data;
-
-    // hardcode for demo only
-    uint32_t memrg = regs->a1;
-
-    /* TODO: Get name of the target proc */
-    // proc_name = libread..
-    int i = 0;
-    data = 0;
-    while (data != -1)
-    {
-        libread(caller, memrg, i, &data);
-        proc_name[i] = data;
-        if (data == -1)
-            proc_name[i] = '\0';
-        i++;
-    }
-    proc_name[i] = '\0';
-
-    printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
-
-    /* TODO: Traverse proclist to terminate the proc
-     *       stcmp to check the process match proc_name
-     */
-    // caller->running_list
-    // caller->mlq_ready_queu
-
-    /* TODO Maching and terminating
-     *       all processes with given
-     *        name in var proc_name
-     */
-    for (int i = 0; i < caller->running_list->size; i++)
-    {
-        struct pcb_t *proc = caller->running_list->proc[i];
-        if (proc && strstr(proc->path, proc_name))
-        {
-            printf("  -> Kill running: %s (PID %d)\n", proc->path, proc->pid);
-            free(proc);
-            caller->running_list->proc[i] = NULL;
-        }
-    }
-
-    for (int prio = 0; prio < MAX_PRIO; prio++)
-    {
-        struct queue_t *q = &caller->mlq_ready_queue[prio];
-        for (int j = 0; j < q->size; j++)
-        {
-            struct pcb_t *proc = q->proc[j];
-            if (proc && strstr(proc->path, proc_name))
-            {
-                printf("  -> Kill ready queue: %s (PID %d)\n", proc->path, proc->pid);
-                free(proc);
-                q->proc[j] = NULL;
-            }
-        }
-    }
-
-    return 0;
-}
+ #include "common.h"
+ #include "syscall.h"
+ #include "stdio.h"
+ #include "libmem.h"
+ 
+ #include "queue.h" //! include more
+ #include <string.h>
+ #include <stdlib.h>
+ 
+ void free_process_memory(struct pcb_t *proc)
+ {
+     for (int i = 0; i < PAGING_MAX_SYMTBL_SZ; i++)
+     {
+         if (proc->mm->symrgtbl[i].rg_start != 0 || proc->mm->symrgtbl[i].rg_end != 0)
+         {
+             libfree(proc, i);
+         }
+     }
+ }
+ 
+ int __sys_killall(struct pcb_t *caller, struct sc_regs *regs)
+ {
+     char proc_name[100];
+     uint32_t data;
+  
+     // hardcode for demo only
+     uint32_t memrg = regs->a1;
+  
+     /* TODO: Get name of the target proc */
+     // proc_name = libread..
+     int i = 0;
+     data = 0;
+     while (i < sizeof(proc_name)-1)
+     {
+         if (libread(caller, memrg, i, &data) != 1) return -1;
+         if ((char)data == '\0') break;
+         proc_name[i] = (char)data;
+         i++;
+     }
+     proc_name[i] = '\0';
+ 
+     if (strlen(proc_name) == 0) return -1;
+ 
+     printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
+  
+     /* TODO: Traverse proclist to terminate the proc
+      *       stcmp to check the process match proc_name
+      */
+     // caller->running_list
+     // caller->mlq_ready_queu
+ 
+     /* TODO Maching and terminating
+      *       all processes with given
+      *        name in var proc_name
+      */
+ 
+     if (caller->running_list)
+     {
+         for (int i = 0; i < caller->running_list->size; i++)
+         {
+             struct pcb_t *proc = caller->running_list->proc[i];
+             if (proc && strstr(proc->path, proc_name))
+             {
+                 printf("  -> Kill running: %s (PID %d)\n", proc->path, proc->pid);
+                 free_process_memory(proc);
+                 caller->running_list->proc[i] = NULL;
+             }
+         }
+     }
+ 
+     if (caller->mlq_ready_queue)
+     {
+         for (int prio = 0; prio < MAX_PRIO; prio++)
+         {
+             struct queue_t *q = &caller->mlq_ready_queue[prio];
+             for (int j = 0; j < q->size; j++)
+             {
+                 struct pcb_t *proc = q->proc[j];
+                 if (proc && strstr(proc->path, proc_name))
+                 {
+                     printf("  -> Kill mlq ready queue: %s (PID %d)\n", proc->path, proc->pid);
+                     free_process_memory(proc);
+                     q->proc[j] = NULL;
+                 }
+             }
+         }
+     }
+ 
+     // Chua ro co can kill ca ready_queue hay khong
+     /*else if (caller->ready_queue)
+     {
+         for (int i = 0; i < caller->ready_queue->size; ++i)
+         {
+             struct pcb_t *p = caller->ready_queue->proc[i];
+             if (p && strstr(p->path, proc_name))
+             {
+                 printf("  -> Kill ready queue: %s (PID %d)\n", p->path, p->pid);
+                 free_process_memory(p);
+                 caller->ready_queue->proc[i] = NULL;
+             }
+         }
+     }*/
+ 
+     return 0;
+ }
+ 
